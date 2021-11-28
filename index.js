a3b70783862be1c9c5e07cf3a12210b8e0ab4d84
aeller-design <70665075+aeller-design@users.noreply.github.com>
2021-11-28 10:57:43 -0800

    tickets being properly fetched
    synchronization issues fixed

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const axios = require('axios');
 const prompt = require("prompt-sync")({ sigint: true });
+const domain = "https://zccticketmachine.zendesk.com/api/v2/tickets.json";
+const email = "aeller916@gmail.com";
+const apiToken = "V4kcMLcYowMO22QSVMyxawdttZQAuEvLzGTG0TTj";
 
 async function fetchAll(){
     try {
-       const response = await axios.get('http://localhost:5000/users');
-       return response.data.users_list;     
+       const response = await axios.get(domain, {
+         headers: {
+           'Authorization': 'Basic ' + Buffer.from(email + '/token:' + apiToken).toString('base64')
+         }
+       });
+       return response.data;     
     }
     catch (error){
        //We're not handling errors. Just logging into the console.
@@ -13,19 +20,33 @@ async function fetchAll(){
     }
  }
 
-var selectionPrompt; 
+ //console.log(email + '/token:' + apiToken)
+ //console.log(Buffer.from(email + '/token:' + apiToken).toString('base64'));
 
-do {
-  selectionPrompt = prompt("Please enter a selection: ");
+async function mainMenu(){
+  var selectionPrompt; 
 
-  if (selectionPrompt == "n") {
-    console.log("n selected");
+  do {
+    selectionPrompt = prompt("Please enter a selection: ");
+
+    if (selectionPrompt === "n") {
+      const jsonlist = await fetchAll();
+      //const jsonStr = JSON.stringify(jsonlist);
+      console.log(jsonlist);
+      
+    }
+    else if (selectionPrompt === "p") {
+      console.log("p selected");
+    }
+    else if (selectionPrompt === "q"){
+      console.log("Quiting Program.  Goodbye.");
+      break;
+    }
+    else {
+      console.log("invalid selection");
+    }
   }
-  else if (selectionPrompt =="p") {
-    console.log("p selected");
-  }
-  else {
-    console.log("invalid selection");
-  }
+  while (true);
 }
-while (selectionPrompt != "q");
+
+mainMenu.call();
