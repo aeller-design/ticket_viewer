c19519f8e94e269cb7db8f5f0878e202c3968647
aeller-design <70665075+aeller-design@users.noreply.github.com>
2021-11-28 14:31:39 -0800

    added pagination to get requests
    integrated get requests with user interface menu as well

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const axios = require('axios');
 const prompt = require("prompt-sync")({ sigint: true });
-const domain = "https://zccticketmachine.zendesk.com/api/v2/tickets.json";
+
+const Startdomain = "https://zccticketmachine.zendesk.com/api/v2/tickets.json?page[size]=25";
+
+// Authorization information
 const email = "aeller916@gmail.com";
 const apiToken = "V4kcMLcYowMO22QSVMyxawdttZQAuEvLzGTG0TTj";
 
-async function fetchAll(){
+// fetches tickets based on domain passed in.  Domains should always specify increments of 25 tickets
+async function fetch25(domain){
     try {
        const response = await axios.get(domain, {
          headers: {
@@ -14,29 +18,78 @@ async function fetchAll(){
        return response.data;     
     }
     catch (error){
-       //We're not handling errors. Just logging into the console.
        console.log(error); 
        return false;         
     }
  }
 
- //console.log(email + '/token:' + apiToken)
- //console.log(Buffer.from(email + '/token:' + apiToken).toString('base64'));
+// Displays viable user options
+function userOptions(pageNum, hasMore){
+  console.log("\n-------------------------------");
+  if(pageNum > 1)
+    console.log("p - previous page of tickets");
+  if(hasMore)
+    console.log("n - next page of tickets");
+  console.log("q - quit program");
+  console.log("-------------------------------");
+}
 
+// Displays message on startup
+function welcomeMessage() {
+  console.log("\n-= WELCOME TO TICKET MACHINE TICKET VIEWER =-")
+}
+
+// input: pageNumber and current list of tickets
+// output: updated list of tickets:
+//    next 25 results, previous 25 results or current results
+// pageNum values map: -1 = previous page; 0 = current page; 1 = next page.
+async function getTickets(pageNum, ticketList) {
+  var nextDomain = null;
+  var nextTicketList = null;
+
+  if(pageNum === 1){
+    nextDomain = ticketList.links.next;
+    nextTicketList = await fetch25(nextDomain);
+    return nextTicketList;
+  }
+  else if(pageNum === -1){
+    prevDomain = ticketList.links.prev;
+    prevTicketList = await fetch25(prevDomain);
+    return prevTicketList;
+  }
+  else if(pageNum === 0){
+    console.log(ticketList);
+    return ticketList;
+  }
+  else
+    console.log("ERROR - pageNum out of bounds")
+}
+
+// Main menu with user input loop
 async function mainMenu(){
-  var selectionPrompt; 
+
+  welcomeMessage();
+
+  var jsonList = await fetch25(Startdomain);
+  
+  var selectionPrompt;
+  var page = 1;
+  var hasMore = (jsonList.meta.has_more)  
+  
+  getTickets(0, jsonList);
 
   do {
+    userOptions(page, hasMore);
     selectionPrompt = prompt("Please enter a selection: ");
-
-    if (selectionPrompt === "n") {
-      const jsonlist = await fetchAll();
-      //const jsonStr = JSON.stringify(jsonlist);
-      console.log(jsonlist);
-      
+    
+    if ((selectionPrompt === "n") && hasMore) {
+      page++;
+      jsonList = await getTickets(1, jsonList);
     }
-    else if (selectionPrompt === "p") {
-      console.log("p selected");
+    else if ((selectionPrompt === "p") && (page > 1)) {
+      console.log("previous page");
+      page--;
+      jsonList = await getTickets(-1, jsonList);
     }
     else if (selectionPrompt === "q"){
       console.log("Quiting Program.  Goodbye.");
@@ -45,8 +98,12 @@ async function mainMenu(){
     else {
       console.log("invalid selection");
     }
+
+    console.log(jsonList);
+    hasMore = (jsonList.meta.has_more);
   }
   while (true);
 }
 
+// starts program
 mainMenu.call();
